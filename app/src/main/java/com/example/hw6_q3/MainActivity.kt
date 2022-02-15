d9ca8610f5e1ba9ebd5ccdb0f6016c594002cf36
ts moosavi <tt.musavi@gmail.com>
2022-02-15 17:28:38 +0330

    binding

--- app/src/main/java/com/example/hw6_q3/MainActivity.kt
+++ app/src/main/java/com/example/hw6_q3/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.hw6_q3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hw6_q3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+//        val view = binding.root
+//        setContentView(view)
+        setContentView(binding.root)
+
     }
 }
